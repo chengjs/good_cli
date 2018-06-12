f913289fe7f59a00e541cc49296e85380d8dc57e
老程 <1006153144@qq.com>
2018-06-12 14:27:45 +0800

    将init变成自己开发项目地址
    将init变成自己开发项目地址

--- command/init.js
+++ command/init.js
@@ -8,20 +8,18 @@ const chalk = require('chalk')
 module.exports = () => { 
  co(function *() { 
     // 处理用户输入 
-      let tplName = yield prompt('Template name: ') 
-      let projectName = yield prompt('Project name: ') 
-      let gitUrl 
-      let branch 
- 
-    if (!config.tpl[tplName]) { 
-        console.log(chalk.red('\n × Template does not exit!')) 
-        process.exit() 
+      // let tplName = yield prompt('Template name: ') 
+      let projectName = yield prompt('Project name<默认为 myapp>: ')
+    if (projectName.length == 0) { 
+        projectName = 'myapp'
+        console.log(chalk.aqua('\n 没有输入项目名字，默认为myapp项目---老程---'));
+        // process.exit() 
     } 
-    gitUrl = config.tpl[tplName].url 
-    branch = config.tpl[tplName].branch 
+    // gitUrl = config.tpl[tplName].url 
+    // branch = config.tpl[tplName].branch 
  
     // git命令，远程拉取项目并自定义项目名 
-    let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}` 
+    let cmdStr = `git clone https://github.com/chengjs/vue-best-app.git ${projectName} && cd ${projectName} ` 
  
     console.log(chalk.white('\n Start generating...')) 
  
@@ -30,7 +28,7 @@ module.exports = () => {
         console.log(error) 
         process.exit() 
       } 
-      console.log(chalk.green('\n √ Generation completed!')) 
+      console.log(chalk.green('\n √ 模板下载完成!')) 
       console.log(`\n cd ${projectName} && npm install \n`) 
       process.exit() 
     }) 
